a57e0206367f7f847534df37d1ab2dab950761d4
kwon, minho <minhob38@gmail.com>
2021-07-24 23:24:22 +0900

    Set pug

--- src/app.js
+++ src/app.js
@@ -2,17 +2,23 @@ const Koa = require("koa");
 const Router = require("@koa/router");
 const logger = require("koa-logger");
 const http = require("http");
+const Pug = require("koa-pug");
+const path = require("path");
 
 const app = new Koa();
 const router = new Router();
+const pug = new Pug({
+  viewPath: path.join(__dirname, "/views"),
+  app: app,
+});
 
 const server = http.createServer(app.callback());
 
-const knex = require("./database/knexSetup");
+// const knex = require("./database/knexSetup");
 
 app.use(logger());
 
-router.get("/", (ctx, next) => (ctx.body = "hello world"));
+router.get("/", (ctx, next) => ctx.render("home"));
 
 app.use(router.routes());
 
