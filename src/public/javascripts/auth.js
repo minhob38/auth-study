6c4708ebef1686028b8b41a4282f1e6f2c773678
kwon, minho <minhob38@gmail.com>
2021-08-14 23:24:11 +0900

    Implement fetch

---
+++ src/public/javascripts/auth.js
@@ -0,0 +1,34 @@
+const $form = document.querySelector(".input-container");
+
+$form.addEventListener("submit", async (ev) => {
+  ev.preventDefault();
+
+  let body, url;
+
+  if (ev.target.mode.value === "sign-up") {
+    url = "/auth/sign-up";
+    body = JSON.stringify({
+      name: ev.target.name.value,
+      email: ev.target.email.value,
+      password: ev.target.password.value,
+    });
+  } else {
+    url = "/auth/sign-in";
+    body = JSON.stringify({
+      email: ev.target.email.value,
+      password: ev.target.password.value,
+    });
+  }
+
+  const _res = await fetch(url, {
+    method: "POST",
+    body,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  const res = await _res.json();
+  console.log(res);
+  // localStorage.setItem("nodejs", "express");
+});
